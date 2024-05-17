1a30485cc013750f403530d606ad5372102e98d7
Obua Denis Daniel <dd.obua>
2024-05-17 14:20:00 +0300

    Test store using deposit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import "./index.css";
 import App from "./App";
+
 import store from "./store";
+import { deposit } from "./features/accounts/accountSlice";
+
+store.dispatch({ type: "account/deposit", payload: 250 });
+console.log(store.getState());
 
 const root = createRoot(document.getElementById("root"));
 root.render(
