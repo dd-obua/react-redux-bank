37ef621600c166a6ea78e0966bca58c024241e53
Obua Denis Daniel <dd.obua>
2024-05-17 14:25:49 +0300

    Import and use react-redux provider

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
-
 import store from "./store";
-import { deposit } from "./features/accounts/accountSlice";
-
-store.dispatch({ type: "account/deposit", payload: 250 });
-console.log(store.getState());
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
