29ea90b23fe600615443c33a23f6da95d277cf74
Obua Denis Daniel <dd.obua>
2024-05-20 13:46:13 +0300

    Fix logical bug in payLoan

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -30,9 +30,9 @@ const accountSlice = createSlice({
     },
 
     payLoan(state, action) {
+      state.balance -= state.loan;
       state.loan = 0;
       state.loanPurpose = "";
-      state.balance -= state.loan;
     },
   },
 });
